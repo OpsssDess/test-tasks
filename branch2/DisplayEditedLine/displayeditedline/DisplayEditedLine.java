febd3a7c035cfff24d4d5ffc8dc1c646d8a59daf
Opsss <OpsssDess@icloud.com>
2019-06-13 20:27:00 +0300

    latest fix

--- branch2/DisplayEditedLine/displayeditedline/DisplayEditedLine.java
+++ branch2/DisplayEditedLine/displayeditedline/DisplayEditedLine.java
@@ -3,13 +3,14 @@ package displayeditedline;
 import java.util.Arrays;
 
 public class DisplayEditedLine {
-
+    
     public static void main(String[] args) {
-
+        
         Line line = new Line();//создаю обьект 
         String input = line.scan();//передаю написаную строку в переменую 
         String[] words = line.splitAndCleanLine(input);//делаю из строки массив и чищу его
         Arrays.sort(words);// сортирую по алфавиту
-        line.showWords(line.editWords(words));
+        line.editLine(words);
+        line.showWords(words);
     }
 }
--- branch2/DisplayEditedLine/displayeditedline/Line.java
+++ branch2/DisplayEditedLine/displayeditedline/Line.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Line {
 
-    public String[] words;
-    public Scanner scanner = new Scanner(System.in);
+    private String[] words;
+    private Scanner scanner = new Scanner(System.in);
 
     public String scan() {
         System.out.println("Введите любые символы:");
@@ -17,20 +17,31 @@ public class Line {
         words = line1.split("\\s+");
         return words;
     }
-
-    public String editWords(String[] words1) {
-        String output = "";
-        for (String word : words1) {
-            String first = word.substring(0, 1).toUpperCase();
-            String all = word.substring(1);
-            output += first + all + " ";
-
+    
+    public void editLine(String[] words) {
+        for (int i = 0; i<words.length; i++) {
+            String first = words[i].substring(0, 1).toUpperCase();
+            String all = words[i].substring(1);
+            words[i] = first + all;
         }
-        return output;
-    }
+    }//более правильный вариант 
 
-    public void showWords(String a) {
-        System.out.println(a);
+//    public String editWords(String[] words1) {
+//        String output = "";
+//        for (String word : words1) {
+//            String first = word.substring(0, 1).toUpperCase();
+//            String all = word.substring(1);
+//            output += first + all + " ";
+//
+//        }
+//        return output;
+//    } мой прежний вариант
+
+    public void showWords(String [] a) {
+        for (String b: a){
+        System.out.print(" "+b);
+        }
+        
     }
 
 }
